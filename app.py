d5e6c91e5e37e4654d7595a3761b3d37ce0c3b8c
joaomartins <joao82@gmail.com>
2023-10-13 01:02:18 +0200

    commit

--- app.py
+++ app.py
@@ -14,50 +14,47 @@ template_theme1 = "spacelab"
 url_theme2 = dbc.themes.DARKLY
 template_theme2 = "darkly"
 
-app = dash.Dash(__name__,
-                use_pages=True,
-                assets_folder = 'assets',
-                external_stylesheets=external_stylesheets, 
-                prevent_initial_callbacks=True
-                )
+app = dash.Dash(
+    __name__,
+    use_pages=True,
+    assets_folder="assets",
+    external_stylesheets=external_stylesheets,
+    prevent_initial_callbacks=True,
+)
 
 server = app.server
 
 theme_switch = ThemeSwitchAIO(aio_id="theme", themes=[url_theme1, url_theme2])
 
-theme_colors = [
-    "primary",
-    "secondary",
-    "success",
-    "warning",
-    "danger",
-    "info",
-    "light",
-    "dark",
-    "link"
-    ]
+theme_colors = ["primary", "secondary", "success", "warning", "danger", "info", "light", "dark", "link"]
 
 
 NavLinks = dbc.Container(
     [
         dbc.Row(
             [
-                dbc.Col([
-                    html.Div([theme_switch], className="d-flex align-items-center my-2"),
-                    html.Div([
-                        dbc.NavLink([
-                            html.Div(page["name"],className="text-light ms-3"),
+                dbc.Col(
+                    [
+                        html.Div([theme_switch], className="d-flex align-items-center my-2"),
+                        html.Div(
+                            [
+                                dbc.NavLink(
+                                    [
+                                        html.Div(page["name"], className="text-light ms-3"),
+                                    ],
+                                    href=page["path"],
+                                    active="exact",
+                                )
+                                for page in dash.page_registry.values()
                             ],
-                            href=page["path"],
-                            active="exact",
-                            
-                        )
-                        for page in dash.page_registry.values()
+                            className="d-flex justify-content-end me-2",
+                        ),
                     ],
-                    className="d-flex justify-content-end me-2")
-                ],
-                className="d-flex justify-content-end align-items-center")
-            ], className="d-flex justify-content-end mb-2")
+                    className="d-flex justify-content-end align-items-center",
+                )
+            ],
+            className="d-flex justify-content-end mb-2",
+        )
     ]
 )
 
@@ -70,16 +67,11 @@ navbar = dbc.Navbar(
                     dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
                     dbc.Col(dbc.NavbarBrand("Plotly", className="ms-2")),
                 ],
-                    align="center",
-                    className="g-0",
-                ),
-            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0, className="m-1"),
-            dbc.Collapse(
-                NavLinks,
-                id="navbar-collapse",
-                is_open=False,
-                navbar=True
+                align="center",
+                className="g-0",
             ),
+            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0, className="m-1"),
+            dbc.Collapse(NavLinks, id="navbar-collapse", is_open=False, navbar=True),
         ]
     ),
     className="m-0 p-0",
@@ -91,16 +83,7 @@ navbar = dbc.Navbar(
 content = html.Div(dash.page_container, id="page-content")
 
 
-app.layout = dbc.Container([
-    dbc.Col(
-        [
-            navbar,
-            content
-        ]
-    )
-],
-    className="d-flex align-items-center m-0 p-0",
-    fluid=True)
+app.layout = dbc.Container([dbc.Col([navbar, content])], className="d-flex align-items-center m-0 p-0", fluid=True)
 
 
 # add callback for toggling the collapse on small screens
--- venv/lib/python3.10/site-packages/aio/__init__.py
+++
@@ -1,2 +0,0 @@
-from .aio_theme_switch import ThemeSwitchAIO
-from .aio_theme_changer import ThemeChangerAIO, template_from_url
